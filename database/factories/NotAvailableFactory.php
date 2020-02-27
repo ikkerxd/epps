aef76b12135d2035a65c35662dff2bd40a398d92
vagrant <vagrant@epps>
2020-02-27 21:06:42 +0000

    proyecto base

---
+++ database/factories/NotAvailableFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\NotAvailable;
+use Faker\Generator as Faker;
+
+$factory->define(NotAvailable::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
