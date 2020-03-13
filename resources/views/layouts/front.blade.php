87b6d88fe033522af8b49293c1d38e081cc692dc
Luis Vega <lvegameza@gmail.com>
2020-03-13 15:16:12 -0500

    agregando pantalla de inicio

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -13,91 +13,60 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-  
-
-    <!-- JS -->
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.slimscroll.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/moment.js') }}"></script>
-    <script src="{{ asset('js/moment-with-locales.js') }}"></script>
-    <script src="{{ asset('js/bootstrap-datetimepicker.min.js') }}"></script>
-    <script src="{{ asset('js/select2.full.min.js') }}"></script>
-    
-      <!-- CSS -->
-      <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css') }}">
-      <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.min.css') }}">
-      <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.standalone.css') }}">
-      <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.standalone.min.css') }}">
-      <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker3.css') }}">
-      <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker3.standalone.css') }}">
-      <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker3.standalone.min.css') }}">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    EPPS
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto"></ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @else  
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="col-md-2">
+                <a class="navbar-brand" href="#">
+                    <img src="{{asset('images/logo.png')}}" width="60"  alt="">
+                  </a>
             </div>
-        </nav>
+            <div class="input-group input-group-lg">               
+                <input type="text" class="form-control" placeholder="Buscar producto" aria-describedby="basic-addon1">
+                <div class="input-group-append">
+                    <button class="btn btn-info btn-lg" type="button"><i class="fas fa-search"></i></button>
+                  </div>
+              </div>
+            <div class="col-md-2">
+                <ul class="navbar-nav float-right">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li><a class="nav-link" href="#"><i class="fas fa-cart-plus fa-2x"></i></a></li>
+                        <li><a class="nav-link" href="{{ route('login') }}"><i class="fas fa-user-circle fa-2x"></i></a></li>                        
+                    @else  
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+          </nav>
 
-        <!-- Scripts js-->
-    
+        <!-- Scripts js-->    
         
         <main class="py-4">
             <div class="container">
-            
-
-            @yield('content')
-            @yield('js')
-            
-            
-            
+                @yield('content')            
             </div>
         </main>
     </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6fd4ed856d.js" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,84 @@
 @extends('layouts.front')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-md-center">                 
-        <div class="col-md-8 text-center">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Buscar producto ...">
-            </div>
+<div class="row justify-content-md-center">                 
+    {{-- <div class="col-md-8 text-center">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Buscar producto ...">
         </div>
+    </div> --}}
 
-        <div class="col-md-12">
-            <div class="form-group">
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img class="d-block w-100" src="{{asset('images/slid_01.jpg')}}" alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                        <img class="d-block w-100" src="{{asset('images/slid_02.jpg')}}" alt="First slide">
-                        </div>                
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+    <div class="col-md-12">
+        <div class="form-group">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>                  
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="{{asset('images/slid_01.jpg')}}" alt="First slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="{{asset('images/slid_02.jpg')}}" alt="Second slide">
+                  </div>
+                
                 </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+        </div>
+    </div>         
+    <div class="col-md-12">        
+        <div class="row">
+            <div class="col-sm">
+                <div class="card">
+                    <img src="{{asset('images/slid_01.jpg')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
             </div>
-        </div>           
-              
+            <div class="col-sm">
+              <div class="card">
+                    <img src="{{asset('images/slid_01.jpg')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-sm">
+              <div class="card">
+                    <img src="{{asset('images/slid_01.jpg')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-sm">
+                <div class="card">
+                      <img src="{{asset('images/slid_01.jpg')}}" class="card-img-top" alt="...">
+                      <div class="card-body">
+                          <h5 class="card-title">Special title treatment</h5>
+                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+              </div>
+          </div>       
+        
+    </div> 
 </div>
-
-
-
 @endsection
